86a15419f41577a53e8773023a8ee5158a65ee27
Josep Segarra Vilalta <josep.segarra@students.salle.url.edu>
2021-04-14 17:35:36 +0200

    PreMenuUI done

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        PreMenuUI preMenuUI = new PreMenuUI();
+
         //TODO put this ReadConfigJson to the controller.
         //Obtains the information from the readConfigJson().
 
-        ReadConfigJson.readConfigJson();
+        //ReadConfigJson.readConfigJson();
 
     }
 }
+ 
