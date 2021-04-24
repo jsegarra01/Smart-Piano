77abc50ce6f8eca2baad9bd7a37cb4e311d89032
Josep Segarra Vilalta <josep.segarra@students.salle.url.edu>
2021-04-24 20:59:52 +0200

    commit for bussiness facade

--- Database/oopd_piano.sql
+++ Database/oopd_piano.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE piano_OOPD;
 
+DROP TABLE IF EXISTS SongPlaylistsT CASCADE;
+DROP TABLE IF EXISTS SongStatisticsGeneralT CASCADE;
+DROP TABLE IF EXISTS SongStatisticsHourlyT CASCADE;
+DROP TABLE IF EXISTS PlaylistT CASCADE;
+DROP TABLE IF EXISTS SongT CASCADE;
+DROP TABLE IF EXISTS UserT CASCADE;
+
 DROP TABLE IF EXISTS SongPlaylists CASCADE;
 DROP TABLE IF EXISTS SongStatisticsGeneral CASCADE;
 DROP TABLE IF EXISTS SongStatisticsHourly CASCADE;
@@ -7,57 +14,67 @@ DROP TABLE IF EXISTS Playlist CASCADE;
 DROP TABLE IF EXISTS Song CASCADE;
 DROP TABLE IF EXISTS User CASCADE;
 
-CREATE TABLE User(
-                     username VARCHAR(255) PRIMARY KEY,
-                     email Varchar(255),
-                     password Varchar(255)
+CREATE TABLE UserT(
+                      username VARCHAR(255) PRIMARY KEY,
+                      email Varchar(255),
+                      password Varchar(255)
 );
 
-CREATE TABLE Song(
-                     songId SERIAL PRIMARY KEY,
-                     songName Varchar(255),
-                     authorsName Varchar(255),
-                     duration FLOAT,
-                     recordingDate DATE,
-                     publicBoolean BOOLEAN,
-                     songFile VARCHAR(255),
-                     username VARCHAR(255),
-                     FOREIGN KEY (username) REFERENCES User(username)
+CREATE TABLE SongT(
+                      songId SERIAL PRIMARY KEY,
+                      songName Varchar(255),
+                      authorsName Varchar(255),
+                      duration FLOAT,
+                      recordingDate DATE,
+                      publicBoolean BOOLEAN,
+                      songFile MEDIUMTEXT,
+                      username VARCHAR(255),
+                      FOREIGN KEY (username) REFERENCES UserT(username)
 );
 
-CREATE TABLE SongStatisticsGeneral(
-                                      songId BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE SongStatisticsGeneralT(
+                                       songId BIGINT UNSIGNED PRIMARY KEY,
+                                       timesPlayed INT,
+                                       minutesPlayed INT,
+                                       numberDifferentUsers INT,
+                                       FOREIGN KEY (songId) REFERENCES SongT(songId)
+);
+
+CREATE TABLE SongStatisticsHourlyT(
+                                      hour INT,
+                                      songId BIGINT UNSIGNED,
                                       timesPlayed INT,
-                                      minutesPlayed INT,
-                                      numberDifferentUsers INT,
-                                      FOREIGN KEY (songId) REFERENCES Song(songId)
+                                      minutesListened INT,
+                                      FOREIGN KEY (songId) REFERENCES SongT(songId)
 );
 
-CREATE TABLE SongStatisticsHourly(
-                                     hour INT PRIMARY KEY,
-                                     songId BIGINT UNSIGNED,
-                                     timesPlayed INT,
-                                     minutesListened INT,
-                                     FOREIGN KEY (songId) REFERENCES Song(songId)
+CREATE TABLE PlaylistT(
+                          playlistId SERIAL PRIMARY KEY,
+                          playlistName VARCHAR(255),
+                          username VARCHAR(255),
+                          FOREIGN KEY (username) REFERENCES UserT(username)
 );
 
-CREATE TABLE Playlist(
-                         playlistId SERIAL PRIMARY KEY,
-                         playlistName VARCHAR(255),
-                         username VARCHAR(255),
-                         FOREIGN KEY (username) REFERENCES User(username)
-);
-
-CREATE TABLE SongPlaylists(
-                              songId BIGINT UNSIGNED,
-                              playlistId BIGINT UNSIGNED PRIMARY KEY,
-                              FOREIGN KEY (songId) REFERENCES Song(songId),
-                              FOREIGN KEY (playlistId) REFERENCES Playlist(playlistId)
+CREATE TABLE SongPlaylistsT(
+                               songId BIGINT UNSIGNED,
+                               playlistId BIGINT UNSIGNED,
+                               FOREIGN KEY (songId) REFERENCES SongT(songId),
+                               FOREIGN KEY (playlistId) REFERENCES PlaylistT(playlistId)
 );
 #INSERT USERS
-INSERT INTO User VALUES ('blayaiai','alex.blay@students.salle.url.edu','viscaelbarca');
-INSERT INTO User VALUES ('MEGAKI295','josep.segarra@students.salle.url.edu','admin');
-INSERT INTO User VALUES ('sergiENP','sergi.vives@students.salle.url.edu','password');
-INSERT INTO User VALUES ('styopartist','stepan.batllori@students.salle.url.edu', 'madrepatriaRussia');
-INSERT INTO User VALUES ('lauranuez','laura.nuez@students.salle.url.edu', 'vivaespanya');
-SELECT * FROM User;
+INSERT INTO UserT VALUES ('blayaiai','alex.blay@students.salle.url.edu','viscaelbarca');
+INSERT INTO UserT VALUES ('MEGAKI295','josep.segarra@students.salle.url.edu','admin');
+INSERT INTO UserT VALUES ('sergiENP','sergi.vives@students.salle.url.edu','password');
+INSERT INTO UserT VALUES ('styopartist','stepan.batllori@students.salle.url.edu', 'madrepatriaRussia');
+INSERT INTO UserT VALUES ('lauranuez','laura.nuez@students.salle.url.edu', 'vivaespanya');
+SELECT * FROM UserT;
+
+INSERT INTO SongT VALUES (1,'Bohemian Rhapsody','Queen','6','2001-03-23',true,'Songs/Queen - Bohemian Rhapsody','lauranuez');
+INSERT INTO SongT VALUES (2, 'Star Wars Theme','John Williams', 5.46,'1977-11-07',true,'Songs/Star-Wars-Theme','blayaiai');
+SELECT * FROM SongT;
+INSERT INTO PlaylistT VALUES (1,'Blayaiais Playlist','blayaiai');
+INSERT INTO SongPlaylistsT VALUES(1,1);
+INSERT INTO SongPlaylistsT VALUES(2,1);
+
+SELECT * FROM SongPlaylistsT;
+SELECT * FROM PlaylistT;
