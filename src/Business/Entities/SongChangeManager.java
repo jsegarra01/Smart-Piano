1497fc61f529cfc15b82b07e3c899bff56262797
Stepan Batllori Martinez <stepan.batllori@students.salle.url.edu>
2021-05-23 15:18:46 +0200

    Song time Change Observer

---
+++ src/Business/Entities/SongChangeManager.java
@@ -0,0 +1,17 @@
+package Business.Entities;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SongChangeManager extends Observer implements ActionListener {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
